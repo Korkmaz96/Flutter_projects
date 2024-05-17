d68af86b367e0dd95caad33a45fd898d8651fef2
Korkmaz96 <mikail-korkmaz@web.de>
2024-05-17 16:26:56 +0200

    changes made regarding color pattern using lineargradiant and started implementing statelesswidget

--- Roll_Dice_app/lib/main.dart
+++ Roll_Dice_app/lib/main.dart
@@ -2,13 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 237, 236, 240),
+                Color.fromARGB(255, 50, 2, 87),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                color: Colors.white, 
+                fontSize: 28,
+                ),
+            ),
+          ),
         ),
       ),
     ),
   );
 }
- 
+
+class GradiantContainer extends StatelessWidget{
+  
+}
